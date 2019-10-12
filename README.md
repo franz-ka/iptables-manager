698c1b116375118542d3508fc60ecb8c39b863b1
wencha <wencha_@hotmail.com>
2019-10-12 16:21:01 -0300

    avances generales; nuevo agregar regla

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Miniprograma para ver más cómodamente las listas de reglas de `iptables`, todas sus tablas y chains asociadas.
 
+Por ahora solo soporta ver las tablas, y agregar regla con: desde ip, protocolo y hacia puerto.
+
 - `sudo apt install python3-pyqt5` para instalar PyQt5 (60MB aprox.)
 - `sudo python3 main.py` para correr.   
 (requiere `sudo` porque usa el comando `iptables` para hacer todas las operaciones)
@@ -13,7 +15,7 @@ Interfaz:
 *Opcional:* `sudo apt install qttools5-dev-tools` para instalar editor visual Qt Designer
 
 Si usás el Qt Designer, una vez exportada una UI a un archivo `*.ui`
-se traduce a python haciendo `pyuic5 EL_ARCHIVO.ui`
+lo exportás al archivo `ui.py` haciendo  `pyuic5 EL_ARCHIVO.ui > ui.py`
 
 Qt Designer:
 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# Referencia de PyQt widgets - https://doc.qt.io/qtforpython/PySide2/QtWidgets/
+
 import subprocess
+import os
 import sys
 import datetime
 import time
@@ -10,12 +13,15 @@ iptable_names = None
 iptable_protocols = ['all', 'tcp', 'udp', 'icmp', 'sctp']
 iptable_acctions = ['ACCEPT', 'DROP', 'REJECT']
 
+def run_cmd_fast(cmd):    
+    os.system(cmd)    
+    window.log_cmd(cmd)
+    
 def run_cmd(cmd):    
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     
-    tstamp = datetime.datetime.now().strftime('%X')
-    window.listLog.insertItem(0, f'[{tstamp}]~$ {cmd}')
+    window.log_cmd(cmd)
     
     return out
 
@@ -35,6 +41,20 @@ class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.comboBox.currentIndexChanged.connect(self.updateChainList)
         
         self.listWidget.currentTextChanged.connect(self.updateRulesList)
+        
+        self.buttonAddRegla.clicked.connect(self.addRegla) 
+        
+    def log_cmd(self, cmd):    
+        tstamp = datetime.datetime.now().strftime('%X')
+        self.listLog.insertItem(0, f'[{tstamp}]~$ {cmd}')
+        
+    def error_msg(self, msg):        
+        msgbox = QtWidgets.QMessageBox(self)
+        msgbox.setIcon(QtWidgets.QMessageBox.Critical)
+        msgbox.setText("Error")
+        msgbox.setInformativeText(msg)
+        msgbox.setWindowTitle("Error")
+        msgbox.show()
         
     def updateDockerExclude(self):
         self.exclude_docker = self.checkBox.isChecked()
@@ -68,6 +88,38 @@ class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
         rules = run_cmd_splitlines(f'sudo iptables -t {selected_table} -S {selected_chain} | {self.exclude_grep}')
         
         self.listWidget_2.addItems(rules)
+        
+    def addRegla(self):
+        selected_table = self.comboBox.currentText()
+        if not selected_table:
+            self.error_msg('Debe seleccionar una tabla y una chain')
+            return
+        
+        selected_chain_item = self.listWidget.currentItem()
+        if not selected_chain_item:
+            self.error_msg('Debe seleccionar una chain')
+            return
+        selected_chain = selected_chain_item.text()
+        
+        conds = []
+        
+        from_ip_or_net = self.editAddIp.text()
+        if from_ip_or_net:
+            conds.append(f'-s {from_ip_or_net}')
+            
+        protocol = self.comboAddProtocolo.currentText()
+        conds.append(f'-p {protocol}')
+        
+        to_port= self.editAddPuerto.text()
+        if to_port:
+            conds.append(f'--dport {to_port}')
+            
+        action = self.comboAddAccion.currentText()        
+        conds.append(f'-j {action}')
+        
+        cmd = f'sudo iptables -t {selected_table} -A {selected_chain} ' + ' '.join(conds)
+        run_cmd_fast(cmd)
+        self.updateRulesList(selected_chain)
 
 
 def main():
@@ -79,7 +131,6 @@ def main():
     
     iptable_names = run_cmd_splitlines('iptables-save | grep "^*" | cut -c2-')
     iptable_names.insert(0, '')
-    #os.system('iptables-save | grep "^*" | cut -c2-')
         
     window.comboBox.addItems(iptable_names)
     window.comboAddProtocolo.addItems(iptable_protocols)
